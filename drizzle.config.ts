db9b478fcfac13a59918008fb14885f61c95268c
doum <notivior@gmail.com>
2025-05-27 14:25:11 +0900

    feat: superbase and drizzle settings

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+// drizzle kit이 정보를 확인할 때 참조하는 파일
+
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./app/features/**/schema.ts",
+  out: "./app/migrations",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
